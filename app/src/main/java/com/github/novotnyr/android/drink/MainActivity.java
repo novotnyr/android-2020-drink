aee4c895d1bb0ec71892f2e20ec0a689cb3c4061
Robert Novotny <novotnr0@gmail.com>
2020-03-11 22:34:05 +0100

    Pridany floating action button aj s obsluhou kliku

--- app/src/main/java/com/github/novotnyr/android/drink/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/drink/MainActivity.java
@@ -1,6 +1,10 @@
 package com.github.novotnyr.android.drink;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView drinkRecyclerView;
 
     DrinkListAdapter drinkListAdapter;
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
         drinkListAdapter = new DrinkListAdapter();
         drinkRecyclerView.setAdapter(drinkListAdapter);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Na zdravie!", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
