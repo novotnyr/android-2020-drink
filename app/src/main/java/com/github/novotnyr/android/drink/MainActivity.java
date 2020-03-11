9d59aa4164a92711e396f580e62f19c121e869ed
Robert Novotny <novotnr0@gmail.com>
2020-03-11 21:34:34 +0100

    Pridany kod widgetov a adapterov

--- app/src/main/java/com/github/novotnyr/android/drink/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/drink/MainActivity.java
@@ -1,14 +1,25 @@
 package com.github.novotnyr.android.drink;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView drinkRecyclerView;
+
+    DrinkListAdapter drinkListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drinkRecyclerView = findViewById(R.id.drinkRecyclerView);
+        drinkRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        drinkListAdapter = new DrinkListAdapter();
+        drinkRecyclerView.setAdapter(drinkListAdapter);
     }
 }
